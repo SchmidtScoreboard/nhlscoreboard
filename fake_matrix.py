0a3e6c5be03b4410ec98eb3aed2e3fb5893f2f26
Mark Schmidt <schmidtwmark@gmail.com>
2019-05-15 17:40:03 -0500

    Begin TK bullshit

--- fake_matrix.py
+++ fake_matrix.py
@@ -1,17 +1,25 @@
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont, ImageTk
 from refresh import *
-class FakeMatrix():
-    
-    def __init__(self, options=None):
-        pass
-    
-    def SetImage(self, image):
-        image = RefreshScreen("text").get_image()
-        image.show()
+import tkinter as tk
+import time
 
+class FakeMatrix(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.window = tk.Canvas(root, width=400, height=400)
+        self.pack()
+        self.window.pack()
+    def SetImage(self, img):
+        img = RefreshScreen("text").get_image()
+        tkimg = ImageTk.PhotoImage(img)
+        self.window.create_image(0, 0, anchor="nw", image=tkimg)
+        self.window.pack()
     def Clear(self):
         pass
 
 if __name__ == "__main__":
-    matrix = FakeMatrix()
-    matrix.SetImage("asdf")
+    root = tk.Tk()
+    matrix = FakeMatrix(root)
+    matrix.SetImage("blah")
+    matrix.mainloop()
--- files.py
+++ files.py
@@ -2,9 +2,9 @@ import os, sys
 root_path = os.path.dirname(os.path.realpath(sys.argv[0]))
 print(root_path)
 settings_path = os.path.join(root_path, "scoreboard_settings.json")
-wpa_template = "/home/pi/nhlscoreboard/wpa_supplicant.conf.template"
-wpa_path = "/home/pi/nhlscoreboard/wpa_supplicant.conf"
-hotspot_on = "/home/pi/nhlscoreboard/hotspot_on.sh"
-hotspot_off = "/home/pi/nhlscoreboard/hotspot_off.sh"
+wpa_template = os.path.join(root_path, "wpa_supplicant.conf.template")
+wpa_path = os.path.join(root_path, "wpa_supplicant.conf")
+hotspot_on = os.path.join(root_path, "hotspot_on.sh")
+hotspot_off = os.path.join(root_path, "hotspot_off.sh")
 small_font = os.path.join(root_path, "fonts/4x6.pil")
 big_font = os.path.join(root_path, "fonts/5x8.pil")
